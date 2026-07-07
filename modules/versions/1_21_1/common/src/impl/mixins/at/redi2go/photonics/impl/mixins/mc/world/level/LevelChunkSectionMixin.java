d42ea8b468c6ed4f7257b6dea7c32300fcd64555
Krzysztof Kalisiak <krzysztof.kalisiak@kearney.com>
2026-07-07 20:04:25 +0200

    another fix

--- modules/versions/1_21_1/common/src/impl/mixins/at/redi2go/photonics/impl/mixins/mc/world/level/LevelChunkSectionMixin.java
+++ modules/versions/1_21_1/common/src/impl/mixins/at/redi2go/photonics/impl/mixins/mc/world/level/LevelChunkSectionMixin.java
@@ -2,13 +2,14 @@ package at.redi2go.photonics.impl.mixins.mc.world.level;
 
 import at.redi2go.photonics.api.mc.world.level.IBlockState;
 import at.redi2go.photonics.api.mc.world.level.chunk.IChunkSection;
+import net.minecraft.core.Holder;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.LevelChunkSection;
-import org.spongepowered.asm.mixin.Implements;
-import org.spongepowered.asm.mixin.Interface;
+import net.minecraft.world.level.chunk.PalettedContainer;
+import net.minecraft.world.level.chunk.PalettedContainerRO;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(LevelChunkSection.class)
 public abstract class LevelChunkSectionMixin implements IChunkSection {
@@ -17,6 +18,12 @@ public abstract class LevelChunkSectionMixin implements IChunkSection {
 
     @Shadow
     public abstract boolean hasOnlyAir();
+
+    @Shadow
+    public abstract PalettedContainer<BlockState> getStates();
+
+    @Shadow
+    public abstract PalettedContainerRO<Holder<Biome>> getBiomes();
 
     @Override
     public IBlockState ph$getBlockState(int x, int y, int z) {
@@ -30,6 +37,6 @@ public abstract class LevelChunkSectionMixin implements IChunkSection {
 
     @Override
     public IChunkSection ph$createCopy() {
-        return (IChunkSection) LevelChunkSectionInvoker.copySection((LevelChunkSection) (Object) this);
+        return (IChunkSection) new LevelChunkSection(getStates().copy(), getBiomes().recreate());
     }
 }
