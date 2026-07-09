a9738a913e1fb2cdc9ab953a6fbc154532d4755a
Krzysztof Kalisiak <krzysztof.kalisiak@kearney.com>
2026-07-09 17:33:45 +0200

    hhhhh

--- modules/core/src/main/java/at/redi2go/photonics/core/iris/extensions/RestirPipeline.java
+++ modules/core/src/main/java/at/redi2go/photonics/core/iris/extensions/RestirPipeline.java
@@ -37,7 +37,7 @@ public class RestirPipeline extends AbstractPhotonicsExtension {
 
         if (DISABLE_RESTIR_LIGHTING_PASSES_FOR_DIAGNOSTICS) {
             if (ENABLE_DIRECT_DIFFUSE_PASS_FOR_DIAGNOSTICS)
-                Photonics.LOGGER.info("Photonics diagnostic: brute-force direct traced light sum with reverse visibility tracing; Photon radiosity preserved; remaining ReSTIR passes disabled");
+                Photonics.LOGGER.info("Photonics diagnostic: brute-force direct traced light sum with reverse visibility tracing and fragment-block self-hit allowance; Photon radiosity preserved; remaining ReSTIR passes disabled");
             else if (ENABLE_INITIAL_DIRECT_PASS_FOR_DIAGNOSTICS)
                 Photonics.LOGGER.info("Photonics diagnostic: initial direct pass only; remaining ReSTIR lighting passes disabled");
             else if (ENABLE_FRAG_DATA_PASS_FOR_DIAGNOSTICS)
