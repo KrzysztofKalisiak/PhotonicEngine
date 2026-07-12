ea6d780b7f30f841e63d011df59b629a2def9b35
Krzysztof Kalisiak <krzysztof.kalisiak@kearney.com>
2026-07-12 13:48:32 +0200

    jkjkjk

--- modules/core/src/main/java/at/redi2go/photonics/core/iris/extensions/RestirPipeline.java
+++ modules/core/src/main/java/at/redi2go/photonics/core/iris/extensions/RestirPipeline.java
@@ -37,7 +37,7 @@ public class RestirPipeline extends AbstractPhotonicsExtension {
 
         if (DISABLE_RESTIR_LIGHTING_PASSES_FOR_DIAGNOSTICS) {
             if (ENABLE_DIRECT_DIFFUSE_PASS_FOR_DIAGNOSTICS)
-                Photonics.LOGGER.info("Photonics diagnostic: direct-light-v7 reservoir accumulation, no reuse");
+                Photonics.LOGGER.info("Photonics diagnostic: direct-light-v8 emissive-cluster visibility, accumulation");
             else if (ENABLE_INITIAL_DIRECT_PASS_FOR_DIAGNOSTICS)
                 Photonics.LOGGER.info("Photonics diagnostic: initial direct pass only; remaining ReSTIR lighting passes disabled");
             else if (ENABLE_FRAG_DATA_PASS_FOR_DIAGNOSTICS)
